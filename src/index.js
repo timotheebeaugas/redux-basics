7f9f9544733302fe1a2cb25cffb63b01223d61b5
Your Name <you@example.com>
2022-02-28 11:51:44 +0100

    post+ user with redux

--- src/index.js
+++ src/index.js
@@ -3,4 +3,27 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Redux
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import { getPosts } from "./actions/post.action";
+import { getUser } from "./actions/user.action";
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk)) // Diasable in production !
+);
+
+store.dispatch(getPosts());
+store.dispatch(getUser());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+ 
